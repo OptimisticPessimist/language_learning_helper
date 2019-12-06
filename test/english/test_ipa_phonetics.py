875944a346788173433fe0ee42138be5c44cb435
OptimisticPessimist <wordswrod.mk@gmail.com>
2019-12-07 01:39:26 +0900

    #2 Fix export <br> tags
    改善案に沿った訂正を行ったところ、他にも辞書に含まれない単語のあとにスペースが追加されない不具合を見つけたので修正しました。
    
    その他、Lの発音記号をよく使われる形に修正

--- test/english/test_ipa_phonetics.py
+++ test/english/test_ipa_phonetics.py
@@ -2,12 +2,27 @@ import pytest
 
 from src.english.ipa_phonetics import *
 
+
 @pytest.mark.parametrize("sentence, expected", [
         ("This is a pen.", [('This', 'ðɪ́s'), ('is', 'ɪ́z'), ('a', 'ə'), ('pen.', 'pɜ́n')]),
-        ("I just read the article on the newspaper.", [("I", 'áɪ'), ("just", 'ʤʌ́st'), ("read", 'rɜ́d'), ("the", 'ðiː'), ("article", 'ɑ́ːrtəkəɫ'), ("on", 'ɑ́ːn'), ("the", 'ðə'), ("newspaper.", 'núzpèɪpɜːr')])
+        ("I just read the article on the newspaper.", [("I", 'áɪ'), ("just", 'ʤʌ́st'), ("read", 'rɜ́d'), ("the", 'ðiː'), ("article", 'ɑ́ːrtəkəl'), ("on", 'ɑ́ːn'), ("the", 'ðə'), ("newspaper.", 'núzpèɪpɜːr')])
 ])
 def test_fetch_phonetics(sentence, expected):
     phonetic = IPA()
 
     actual = phonetic._fetch_phonetics(sentence)
+    assert actual == expected
+
+
+@pytest.mark.parametrize("word_list, expected", [
+        ([('This', 'ðɪ́s')],
+         ['<ruby class="under">This<rp>[</rp><rt>ðɪ́s</rt><rp>]</rp></ruby> ']),
+        ([("the", 'ðiː'), ("article", 'ɑ́ːrtəkəl')],
+         ['<ruby class="under">the<rp>[</rp><rt>ðiː</rt><rp>]</rp></ruby> ',
+          '<ruby class="under">article<rp>[</rp><rt>ɑ́ːrtəkəl</rt><rp>]</rp></ruby> ']),
+])
+def test_put_on(word_list, expected):
+    phonetic = IPA()
+
+    actual = phonetic._put_on(word_list)
     assert actual == expected
